fd8fa335ec4ef212fb3dad7c28b2781401691c95
Vlad <vlad.popovschii@gmail.com>
2022-07-24 13:43:08 +0300

    Refactored configs

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+TELEGRAM_BOT_TOKEN=os.getenv('TELEGRAM_BOT_TOKEN')
+MONGO_URI=os.getenv('MONGO_URI')
--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from operator import imod
 from aiogram import Bot, Dispatcher, executor, types
 import logging
